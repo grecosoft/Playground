97788ef2bf8deec4f0899de30486aedf82a45067
Brian Greco <blgreco72@gmail.com>
2026-01-29 10:05:12 -0500

    Added starting workflow to test azure federated identity

--- iac/config/modules/github/main.tf
+++ iac/config/modules/github/main.tf
@@ -5,8 +5,17 @@ resource "azurerm_user_assigned_identity" "github_actions" {
   name                = var.identity_name
 }
 
+resource "azurerm_federated_identity_credential" "github_main_branch" {
+  name                = "github-build-federated-credential"
+  resource_group_name = var.resource_group_name
+  parent_id           = azurerm_user_assigned_identity.github_actions.id
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = "https://token.actions.githubusercontent.com"
+  subject             = "repo:${var.github_account_name}/${var.solution_name}:ref:refs/heads/main"
+}
+
 resource "azurerm_federated_identity_credential" "github_actions" {
-  name                = "github-actions-federated-credential"
+  name                = "github-${var.environment}-federated-credential"
   resource_group_name = var.resource_group_name
   parent_id           = azurerm_user_assigned_identity.github_actions.id
   audience            = ["api://AzureADTokenExchange"]
