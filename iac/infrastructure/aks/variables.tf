6c61599554e76628433c03cbfa51233684397023
Brian Greco <blgreco72@gmail.com>
2026-02-09 14:04:15 -0500

    research

--- iac/infrastructure/aks/variables.tf
+++ iac/infrastructure/aks/variables.tf
@@ -1,14 +1,14 @@
 variable "subscription_id" {
   description = "The Subscription ID where the resources will be created."
-  type = string
+  type        = string
 }
 
 variable "resource_group_name" {
   description = "The resource group to create service account for Terraform state storage."
-  type = string
+  type        = string
 }
 
 variable "storage_account_name" {
   description = "The storage account name to create for Terraform state storage."
-  type = string
+  type        = string
 }
