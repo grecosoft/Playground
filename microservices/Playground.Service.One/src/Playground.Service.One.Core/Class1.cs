a9f447b2287b651978af731078c72f49baee3984
Brian Greco <blgreco72@gmail.com>
2026-02-20 16:35:26 -0500

    docker

---
+++ microservices/Playground.Service.One/src/Playground.Service.One.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Playground.Service.One.Core;
+
+public class Class1
+{
+
+}
---
+++ microservices/Playground.Service.One/src/Playground.Service.One.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Playground.Service.One.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
