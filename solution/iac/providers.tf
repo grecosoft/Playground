ea704831a814cbba9da8788c5ae125a0453fd88b
Brian Greco <blgreco72@gmail.com>
2026-04-09 15:24:34 -0400

    qqwws

--- solution/iac/providers.tf
+++ solution/iac/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.4"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.1"
+    }
   }
   backend "azurerm" {
     key = "solution.tfstate"
@@ -13,4 +17,14 @@ terraform {
 provider "azurerm" {
   subscription_id = var.subscription_id
   features {}
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.azurerm_kubernetes_cluster.k8s.kube_config.0.host
+
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
+  }
 }
