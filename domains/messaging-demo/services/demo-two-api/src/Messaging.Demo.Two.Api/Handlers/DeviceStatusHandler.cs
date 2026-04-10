ff111692472ab2b393bb3c791d257ab9af9efad4
Brian Greco <blgreco72@gmail.com>
2026-04-10 10:56:56 -0400

    &&

--- domains/messaging-demo/services/demo-two-api/src/Messaging.Demo.Two.Api/Handlers/DeviceStatusHandler.cs
+++ domains/messaging-demo/services/demo-two-api/src/Messaging.Demo.Two.Api/Handlers/DeviceStatusHandler.cs
@@ -1,14 +1,14 @@
-﻿using Common.Messaging.Commands;
-using Messaging.Demo.Common.Commands;
-
-namespace Messaging.Demo.Two.Api.Handlers;
-
-public class DeviceStatusHandler : CommandHandlerBase<DeviceUpdateCommand, DeviceStatus>
-{
-    protected override async Task HandleMessage(DeviceUpdateCommand command,
-        CommandContext context,
-        CancellationToken cancellationToken)
-    {
-        await context.CommandRepository.SaveCommand(context, cancellationToken);
-    }
-}
+﻿// using Common.Messaging.Commands;
+// using Messaging.Demo.Common.Commands;
+//
+// namespace Messaging.Demo.Two.Api.Handlers;
+//
+// public class DeviceStatusHandler : CommandHandlerBase<DeviceUpdateCommand, DeviceStatus>
+// {
+//     protected override async Task HandleMessage(DeviceUpdateCommand command,
+//         CommandContext context,
+//         CancellationToken cancellationToken)
+//     {
+//         await context.CommandRepository.SaveCommand(context, cancellationToken);
+//     }
+// }
--- domains/messaging-demo/services/demo-two-api/src/Messaging.Demo.Two.Api/Handlers/PingHandler.cs
+++ domains/messaging-demo/services/demo-two-api/src/Messaging.Demo.Two.Api/Handlers/PingHandler.cs
@@ -1,17 +1,17 @@
-﻿using Common.Messaging.Commands;
-using Messaging.Demo.Common.Commands;
-
-namespace Messaging.Demo.Two.Api.Handlers;
-
-public class PingMessageHandler : CommandHandlerBase<PingCommand, PingResponse>
-{
-    protected override Task HandleMessage(
-        PingCommand command,
-        CommandContext context,
-        CancellationToken cancellationToken)
-    {
-
-        context.SetResponse(new PingResponse($"Pong-{command.ValueOne}/{command.ValueOne}-{Guid.NewGuid()}"));
-        return Task.CompletedTask;
-    }
-}
+﻿// using Common.Messaging.Commands;
+// using Messaging.Demo.Common.Commands;
+//
+// namespace Messaging.Demo.Two.Api.Handlers;
+//
+// public class PingMessageHandler : CommandHandlerBase<PingCommand, PingResponse>
+// {
+//     protected override Task HandleMessage(
+//         PingCommand command,
+//         CommandContext context,
+//         CancellationToken cancellationToken)
+//     {
+//
+//         context.SetResponse(new PingResponse($"Pong-{command.ValueOne}/{command.ValueOne}-{Guid.NewGuid()}"));
+//         return Task.CompletedTask;
+//     }
+// }
